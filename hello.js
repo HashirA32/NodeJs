99f9a0ffc3992482635633a1dbd2ccdeb04d8808
HashirA32 <hashirofficiala32@gmail.com>
2024-12-24 01:35:06 -0800

    Node modules

---
+++ hello.js
@@ -0,0 +1,5 @@
+let Greeting = function (counts) {
+    console.log("there are " + counts.length + " elements in the arr")
+}
+
+module.exports = Greeting;
---
+++ index.js
@@ -0,0 +1,39 @@
+// How to make a server
+
+
+
+// let http = require('http');
+// let server = http.createServer(( request, responce) => {
+// console.log("You hit a request.")
+// });
+// server.listen('3000', 'localhost', () => {
+//     console.log('I am listning')
+// });
+
+
+
+// Accessing the Global objects
+
+
+// console.log("hy Hashir")
+
+// setTimeout(function(){
+//     console.log("10seconda have passed.")
+// }, 10000)
+
+// let time = 0;
+// let Timer = setInterval(function(){
+//     time += 1;
+//     console.log(time +"seconda have passed.")
+//     if(time >= 5){
+//         clearTimeout(Timer)
+//     }
+// }, 1000)
+// console.log(__filename);
+
+
+// Node Modules
+
+let lenght = require('./hello');
+
+console.log(lenght(['h', 'a', 's', 'h', 'h', 'i', 'i', 'r']));
